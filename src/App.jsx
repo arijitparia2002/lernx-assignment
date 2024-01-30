0a687798c3abaf050eab1c784374b0db9a17f954
Arijit Paria <arijitparia2020@gmail.com>
2024-01-30 16:28:33 +0530

    init: initiallize app and add components

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      Navbar 
+      Menus
+      CommunityFeed 
+      Footer
+      Profile 
+      Tabs
+      Links 
+      Ranking 
+      WheelOfOpp 
+      
+    </div>
+  )
+}
+
+export default App
